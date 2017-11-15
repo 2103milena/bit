6012d7fd02f833fd34d19e6bd2a51dcaf677b15d
BIT Student <classroom@bgit.rs>
2017-11-15 18:09:11 +0100

    blog i css

---
+++ bit-cs/reactRecreate/components/main.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Post from "./post";
+import Authors from "./authors";
+import About from "./about";
+import {Link} from 'react-router-dom';
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { posts: [] };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(posts => this.setState({ posts }))
+    }
+
+    render() {
+        const posts = this.state.posts;
+       
+
+        if (posts.length === 0) {
+            return <p>No posts</p>
+        }
+
+        return (
+            <div>
+               {posts.map((item) => {
+                    return <Post post={item} key={item.id} />                
+                })}
+                
+
+
+            </div>
+        )
+    }
+}
+
+export default Main;
---
+++ bit-cs/reactRecreate/components/post.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
+
+const Post = (props) => {
+    return (
+    <Link to={"/singlepost/" + props.post.id}>
+        <div className="col-12 posts">
+              
+            <div className="border">
+                <h2>{props.post.title}</h2>
+                <p>{props.post.body}</p>
+                
+                <hr />
+            </div>
+          
+        </div>
+        </Link>
+        
+    );
+};
+
+
+// Post.propTypes = {
+//     post: PropTypes.shape({
+//         userId: PropTypes.number,
+//         id: PropTypes.number,
+//         title: PropTypes.string,
+//         body: PropTypes.string
+//     }).isRequired
+// };
+
+export default Post;
